142ab653100143e9182f83ed0781b1d7e719bfe8
mukesh.a.vast@tsgforce.com <mukesh.a.vast@tsgforce.com>
2024-10-04 16:23:28 +0530

    Initial Next JS Skeleton out of create-next-app@latest

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Image from "next/image";
+import { Contacts } from "@/components/ui/contacts";
+import { Back } from "@/components/ui/back"
+import { Footer } from "@/components/ui/footer"
+
+export default function ContactsHome() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+            <Contacts/>
+            <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+              <li>Feel free to write.</li>
+            </ol>
+            <Back/>
+
+          </main>
+          <Footer/>
+        </div>
+
+  );
+}
---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge"
+
+export default function Footer() {
+  return (
+
+      <footer className="row-start-3 flex gap-6 flex-wrap items-right justify-right">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+        Powered by →
+          <Image
+                className="dark:invert"
+                src="https://nextjs.org/icons/next.svg"
+                alt="Next.js logo"
+                width={150}
+                height={30}
+                priority
+              />
+
+        </a>
+      </footer>
+
+  );
+}
+
+export { Footer }
