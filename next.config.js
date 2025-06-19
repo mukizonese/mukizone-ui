032c4c4dc38b7b51a6e31d380d890c8ee89ad654
mukesh.a.vast@tsgforce.com <mukesh.a.vast@tsgforce.com>
2025-06-19 14:34:14 +0530

    blogs added

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/tradedemo',
+        //destination: 'http://localhost:3002/',
+        destination: process.env.NEXT_PUBLIC_DEMO_TRADING_URL,
+        permanent: true,
+      },
+      // Wildcard path matching
+      {
+        source: '/blog/:slug',
+        destination: '/news/:slug',
+        permanent: true,
+      },
+    ]
+  },
+  output: "standalone"
+}
